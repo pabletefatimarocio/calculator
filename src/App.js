17cd59dc9f818ee7740428e65de276f2efc481c4
pabletefatimarocio <pabletefatima9@gmail.com>
2022-11-24 16:42:15 -0300

    avances

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import freeCodeCampLogo from './img/freecodecamp-logo.png'
 import Boton from './components/Boton';
 import Pantalla from './components/Pantalla';
 import BotonClear from './components/BotonClear';
@@ -22,12 +21,7 @@ function App() {
 
   return (
     <div className='App'>
-    <div className='freecodecamp-logo-contenedor'>
-       <img
-       src={freeCodeCampLogo} 
-       className='freecodecamp-logo'
-       alt='Logo de freeCodeCamp' />
-    </div>
+    
     <div className='contenedor-calculadora'>
       <Pantalla input={input}/>
        <div className='fila'>
